01f843f93ce2e35bc2fd20949d3831971ffe5df3
mehedikaiser08 <mehedikaiser08@gmail.com>
2018-11-05 09:20:07 +0600

    Stock Management System

---
+++ StackManagementSystemUI/StackManagementSystemUI/ItemForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace StackManagementSystemUI
+{
+    partial class ItemForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.categoryItemComboBox = new System.Windows.Forms.ComboBox();
+            this.companyItemComboBox = new System.Windows.Forms.ComboBox();
+            this.itemNameTextBox = new System.Windows.Forms.TextBox();
+            this.reorderItemLevelTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Category";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Company";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Item Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Reorder Lavel";
+            // 
+            // categoryItemComboBox
+            // 
+            this.categoryItemComboBox.FormattingEnabled = true;
+            this.categoryItemComboBox.Location = new System.Drawing.Point(111, 22);
+            this.categoryItemComboBox.Name = "categoryItemComboBox";
+            this.categoryItemComboBox.Size = new System.Drawing.Size(193, 21);
+            this.categoryItemComboBox.TabIndex = 4;
+            // 
+            // companyItemComboBox
+            // 
+            this.companyItemComboBox.FormattingEnabled = true;
+            this.companyItemComboBox.Location = new System.Drawing.Point(111, 55);
+            this.companyItemComboBox.Name = "companyItemComboBox";
+            this.companyItemComboBox.Size = new System.Drawing.Size(193, 21);
+            this.companyItemComboBox.TabIndex = 5;
+            // 
+            // itemNameTextBox
+            // 
+            this.itemNameTextBox.Location = new System.Drawing.Point(111, 84);
+            this.itemNameTextBox.Name = "itemNameTextBox";
+            this.itemNameTextBox.Size = new System.Drawing.Size(193, 20);
+            this.itemNameTextBox.TabIndex = 6;
+            // 
+            // reorderItemLevelTextBox
+            // 
+            this.reorderItemLevelTextBox.Location = new System.Drawing.Point(111, 120);
+            this.reorderItemLevelTextBox.Name = "reorderItemLevelTextBox";
+            this.reorderItemLevelTextBox.Size = new System.Drawing.Size(193, 20);
+            this.reorderItemLevelTextBox.TabIndex = 7;
+            this.reorderItemLevelTextBox.Text = "0";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(220, 169);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // ItemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 233);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.reorderItemLevelTextBox);
+            this.Controls.Add(this.itemNameTextBox);
+            this.Controls.Add(this.companyItemComboBox);
+            this.Controls.Add(this.categoryItemComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ItemForm";
+            this.Text = "ItemForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox categoryItemComboBox;
+        private System.Windows.Forms.ComboBox companyItemComboBox;
+        private System.Windows.Forms.TextBox itemNameTextBox;
+        private System.Windows.Forms.TextBox reorderItemLevelTextBox;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
---
+++ StackManagementSystemUI/StackManagementSystemUI/ViewSalesForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace StackManagementSystemUI
+{
+    partial class ViewSalesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fromDateComboBox = new System.Windows.Forms.ComboBox();
+            this.toDateComboBox = new System.Windows.Forms.ComboBox();
+            this.viewSalesDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.viewSalesDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(368, 78);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 0;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "From Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To Date";
+            // 
+            // fromDateComboBox
+            // 
+            this.fromDateComboBox.FormattingEnabled = true;
+            this.fromDateComboBox.Location = new System.Drawing.Point(264, 16);
+            this.fromDateComboBox.Name = "fromDateComboBox";
+            this.fromDateComboBox.Size = new System.Drawing.Size(188, 21);
+            this.fromDateComboBox.TabIndex = 3;
+            // 
+            // toDateComboBox
+            // 
+            this.toDateComboBox.FormattingEnabled = true;
+            this.toDateComboBox.Location = new System.Drawing.Point(264, 51);
+            this.toDateComboBox.Name = "toDateComboBox";
+            this.toDateComboBox.Size = new System.Drawing.Size(188, 21);
+            this.toDateComboBox.TabIndex = 4;
+            // 
+            // viewSalesDataGridView
+            // 
+            this.viewSalesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.viewSalesDataGridView.Location = new System.Drawing.Point(22, 142);
+            this.viewSalesDataGridView.Name = "viewSalesDataGridView";
+            this.viewSalesDataGridView.Size = new System.Drawing.Size(567, 199);
+            this.viewSalesDataGridView.TabIndex = 5;
+            // 
+            // ViewSalesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 392);
+            this.Controls.Add(this.viewSalesDataGridView);
+            this.Controls.Add(this.toDateComboBox);
+            this.Controls.Add(this.fromDateComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchButton);
+            this.Name = "ViewSalesForm";
+            this.Text = "View Sales Between Two Dates";
+            ((System.ComponentModel.ISupportInitialize)(this.viewSalesDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox fromDateComboBox;
+        private System.Windows.Forms.ComboBox toDateComboBox;
+        private System.Windows.Forms.DataGridView viewSalesDataGridView;
+    }
+}
